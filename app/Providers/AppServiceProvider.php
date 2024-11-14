2e3c3e0dabc5a39f7bc69ef380350cc10eaae701
anne-maina <annemaina022@gmail.com>
2024-11-14 11:49:53 +0300

    Completed blade tasks

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+         // Pass $metaTitle as "Blade Test" to all views
+         view()->share('metaTitle', 'Blade Test');
     }
 }
